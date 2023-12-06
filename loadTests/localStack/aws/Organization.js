1aef507a878787a84b5602294720e4d7db4ec961
Nof Hasson <nof.hasson@xmcyber.com>
2023-12-06 11:20:14 +0200

    format

--- loadTests/localStack/aws/Organization.js
+++ loadTests/localStack/aws/Organization.js
@@ -2,44 +2,44 @@ import AWS from 'aws-sdk';
 import { defaultAwsConfig } from '../config/config.js';
 
 export default class Organization {
-    constructor() {
-        this.accounts = [];
-        AWS.config.update(defaultAwsConfig);
-        this.organizations = new AWS.Organizations();
-    }
+	constructor() {
+		this.accounts = [];
+		AWS.config.update(defaultAwsConfig);
+		this.organizations = new AWS.Organizations();
+	}
 
-    async createOrganization() {
-        try {
-            const response = await this.organizations.createOrganization({
-                FeatureSet: 'ALL' // LocalStack supports 'ALL' feature set
-            }).promise();
-            console.log('Organization created:', response.Organization);
-            return response.Organization;
-        } catch (error) {
-            console.error('Error creating organization:', error);
-            throw error;
-        }
-    }
+	async createOrganization() {
+		try {
+			const response = await this.organizations.createOrganization({
+				FeatureSet: 'ALL' // LocalStack supports 'ALL' feature set
+			}).promise();
+			console.log('Organization created:', response.Organization);
+			return response.Organization;
+		} catch (error) {
+			console.error('Error creating organization:', error);
+			throw error;
+		}
+	}
 
-    async addAccount(accountConfig) {
-        try {
-            const createAccountResponse = await this.organizations.createAccount({
-                Email: `account+${accountConfig.accountId}@example.com`,
-                AccountName: `Account-${accountConfig.accountId}`
-            }).promise();
+	async addAccount(accountConfig) {
+		try {
+			const createAccountResponse = await this.organizations.createAccount({
+				Email: `account+${accountConfig.accountId}@example.com`,
+				AccountName: `Account-${accountConfig.accountId}`
+			}).promise();
 
-            console.log('Account creation initiated:', createAccountResponse.CreateAccountStatus);
+			console.log('Account creation initiated:', createAccountResponse.CreateAccountStatus);
 
-            // Since LocalStack mocks AWS, the account creation is instantaneous
-            // In a real AWS environment, you would need to poll for account creation status
+			// Since LocalStack mocks AWS, the account creation is instantaneous
+			// In a real AWS environment, you would need to poll for account creation status
 
-            this.accounts.push(createAccountResponse.CreateAccountStatus);
-            console.log('Account added to organization:', createAccountResponse.CreateAccountStatus);
-        } catch (error) {
-            console.error('Error adding account to organization:', error);
-            throw error;
-        }
-    }
+			this.accounts.push(createAccountResponse.CreateAccountStatus);
+			console.log('Account added to organization:', createAccountResponse.CreateAccountStatus);
+		} catch (error) {
+			console.error('Error adding account to organization:', error);
+			throw error;
+		}
+	}
 
-    // Additional methods as required for managing the organization
+	// Additional methods as required for managing the organization
 }
--- loadTests/localStack/init-aws-accounts.js
+++ loadTests/localStack/init-aws-accounts.js
@@ -3,22 +3,22 @@ import { awsConfig } from './aws/awsAccountConfigs.js';
 import Organization from './aws/Organization.js';
 
 export async function initAWSAccounts() {
-    const organization = new Organization();
-    await organization.createOrganization(); // Create the organization
+	const organization = new Organization();
+	await organization.createOrganization(); // Create the organization
 
-    const accountManager = new AWSAccountManager();
+	const accountManager = new AWSAccountManager();
 
-    for (const config of awsConfig) {
-        try {
-            // Create and add account to the organization
-            await organization.addAccount(config);
-            console.log(`Account added to organization with config: ${config.accountId}`);
+	for (const config of awsConfig) {
+		try {
+			// Create and add account to the organization
+			await organization.addAccount(config);
+			console.log(`Account added to organization with config: ${config.accountId}`);
 
-            // Initialize account in AWSAccountManager for further management
-            const accountIdentifier = await accountManager.initializeAccount(config.roleArn, config);
-            console.log(`Account initialized in AWSAccountManager with identifier: ${accountIdentifier}`);
-        } catch (error) {
-            console.error(`Error in account initialization:`, error);
-        }
-    }
+			// Initialize account in AWSAccountManager for further management
+			const accountIdentifier = await accountManager.initializeAccount(config.roleArn, config);
+			console.log(`Account initialized in AWSAccountManager with identifier: ${accountIdentifier}`);
+		} catch (error) {
+			console.error(`Error in account initialization:`, error);
+		}
+	}
 }
