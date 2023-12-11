c80b1bab9f7297254bf16a290415621d7cdbd8c2
Nof Hasson <nof.hasson@xmcyber.com>
2023-12-11 13:23:43 +0200

    Some changes

--- loadTests/localStack/aws/awsAccountConfigs.js
+++ loadTests/localStack/aws/awsAccountConfigs.js
@@ -1,4 +1,4 @@
-export const numberOfAccounts = 400;
+export const numberOfAccounts = 10;
 
 export const awsConfig = [
 	{
--- loadTests/localStack/retrieveOrganization.js
+++ loadTests/localStack/retrieveOrganization.js
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk';
 
+// Configure AWS SDK to use LocalStack
 AWS.config.update({
     endpoint: 'http://localhost:4566',
     region: 'us-east-1',
@@ -7,16 +8,44 @@ AWS.config.update({
     s3ForcePathStyle: true,
 });
 
-const organizations = new AWS.Organizations();
+// Initialize STS client
+const sts = new AWS.STS({ endpoint: 'http://localhost:4566' });
 
-async function getOrganizationInfo() {
-	try {
-		console.log('Retrieving organization info...');
-		const response = await organizations.describeOrganization().promise();
-		console.log('Organization Info:', response.Organization);
-	} catch (error) {
-		console.error('Error retrieving organization info:', error);
-	}
+async function getTemporaryCredentials() {
+    const params = {
+        RoleArn: 'arn:aws:iam::123456789012:role/YourRoleName',
+        RoleSessionName: 'LocalStackSession',
+    };
+    return sts.assumeRole(params).promise();
 }
 
-getOrganizationInfo();
+async function getOrganizationInfo(credentials) {
+    const organizations = new AWS.Organizations({
+        credentials: credentials,
+        region: 'us-east-1',
+    });
+
+    try {
+        console.log('Retrieving organization info...');
+        const response = await organizations.describeOrganization().promise();
+        console.log('Organization Info:', response.Organization);
+    } catch (error) {
+        console.error('Error retrieving organization info:', error);
+    }
+}
+
+async function main() {
+    try {
+        const assumedRole = await getTemporaryCredentials();
+        const temporaryCredentials = new AWS.Credentials({
+            accessKeyId: assumedRole.Credentials.AccessKeyId,
+            secretAccessKey: assumedRole.Credentials.SecretAccessKey,
+            sessionToken: assumedRole.Credentials.SessionToken,
+        });
+        await getOrganizationInfo(temporaryCredentials);
+    } catch (error) {
+        console.error('Error assuming role:', error);
+    }
+}
+
+main();
